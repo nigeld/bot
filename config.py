f48c6ac9dd085a61b356916589591576060c2fa9
Nigel Davila <nayeldavila@gmail.com>
2017-04-03 20:51:08 -0500

    Arreglando errores y configurando webhook

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
-class Config(objec):
-    SECRET_KEY = 'my_secret_token'
-    PAGE_ACCESS_TOKEN = 'token_facebook'
+class Config(object):
+    PAGE_ACCESS_TOKEN = 'EAAFmTsKOqPMBANQZBxJV27WSZBHqS3VZB5yQ6HiOwTaE212IzPM9IpKcMXqBdq8TdbNIgZBVpgnSfuCSdzW68nOke4tcXqK7Dlkx0ZBNPkiDEmfU8n3ZB5GdnmDXFG0DyAeeMzJWHHr09YC4dZA5k9vDGFx7cQIgqrft3kY7jAZCKQZDZD'
+    SECRET_KEY = 'token_facebook_nigel_bot'
 
 class DevelopmentConfig(Config):
     DEBUG = True
--- manage.py
+++ manage.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from flask import request
 from config import DevelopmentConfig
 
 app = Flask(__name__)
-app.config.from_objec( DevelopmentConfig )
+app.config.from_object( DevelopmentConfig )
 
+@app.route("/webhook", methods = ['GET'])
+def webhook():
+    verify_token = request.args.get('hub.verify_token', '')
+    if verify_token == app.config['SECRET_KEY']:
+        return request.args.get('hub.challenge', '')
+    return 'Error al validar el secreto'
 
-@app.route("/", method = ['GET'])
+@app.route("/", methods = ['GET'])
 def index():
     return 'Bienvenido a la pagina del bot'
 
