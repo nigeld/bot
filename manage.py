b5e99c668586d59ed3b4d0aa64f203276b2b288e
Nigel Davila <nayeldavila@gmail.com>
2017-04-03 21:12:45 -0500

    Recibiendo los mensajes

--- manage.py
+++ manage.py
@@ -1,16 +1,28 @@
 from flask import Flask
 from flask import request
+
+import json
 from config import DevelopmentConfig
 
 app = Flask(__name__)
 app.config.from_object( DevelopmentConfig )
 
-@app.route("/webhook", methods = ['GET'])
+@app.route("/webhook", methods = ['GET', 'POST'])
 def webhook():
-    verify_token = request.args.get('hub.verify_token', '')
-    if verify_token == app.config['SECRET_KEY']:
-        return request.args.get('hub.challenge', '')
-    return 'Error al validar el secreto'
+    if request.method == 'GET':
+        verify_token = request.args.get('hub.verify_token', '')
+        if verify_token == app.config['SECRET_KEY']:
+            return request.args.get('hub.challenge', '')
+        return 'Error al validar el secreto'
+    elif request.method == 'POST':
+        payload = request.get_data()
+        data = json.loads(payload)
+        for page_entry in data['entry']:
+            for message_event in page_entry['messaging']:
+                if 'message' in message_event:
+                    evento = message_event['message']
+                    print(evento['text'])
+        return 'ok'
 
 @app.route("/", methods = ['GET'])
 def index():
